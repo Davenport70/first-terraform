e380d48f6ac83b34f59c0a7d41f5f0be60bd7ca3
Zack Davenport <zackdavenport@tcadmins-Air.broadband>
2020-04-24 15:33:43 +0100

    edits to the main.tf

--- main.tf
+++ main.tf
@@ -15,24 +15,53 @@ provider "aws" {
 # create new subnet
 # move our instance into the subnet
 resource "aws_subnet" "app_subnet" {
-    vpc_id = "vpc-07e47e9d90d2076da"
+    vpc_id = var.vpc_id
     cidr_block = "172.31.50.0/24"
     availability_zone = "eu-west-1a"
     tags = {
-      Name = "zack-eng54-public-subnet"
+      Name = var.name
     }
+}
+
+# Route table
+resource "aws_route_table" "public" {
+  # it is associated with subnet_id
+  vpc_id = var.vpc_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = data.aws_internet_gateway.default-gw.id
+  }
+  tags = {
+    Name = "${var.name}-public-table"
+  }
+}
+
+resource "aws_route_table_association" "assoc" {
+  subnet_id = aws_subnet.app_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+# we don't need a new IG -
+# we can query our existing vpc/infrastructure with the 'data' handler function
+data "aws_internet_gateway" "default-gw" {
+  filter {
+    # on the hashicorp docs, it erferences AWS-API that has this filer "attachment.vpc-id"
+    name = "attachment.vpc-id"
+    values = [var.vpc_id]
+
+  }
 }
 
 # Launching an instance
 
 resource "aws_instance" "app_instance" {
-    ami = "ami-040bb941f8d94b312"
+    ami = var.ami_id
     instance_type = "t2.micro"
     associate_public_ip_address = true
     subnet_id = aws_subnet.app_subnet.id
     vpc_security_group_ids = [aws_security_group.app_sg.id]
     tags = {
-        Name = "eng54-zack-terraform"
+        Name = var.name
     }
 }
 
@@ -40,10 +69,10 @@ resource "aws_instance" "app_instance" {
 resource "aws_security_group" "app_sg" {
   name        = "zack-app-security-group"
   description = "Allow 80 inbound traffic"
-  vpc_id      = "vpc-07e47e9d90d2076da"
+  vpc_id      = var.vpc_id
 
   tags = {
-    Name = "zack-app-sg-tags"
+    Name = "${var.name}-tags"
   }
 
   ingress {
